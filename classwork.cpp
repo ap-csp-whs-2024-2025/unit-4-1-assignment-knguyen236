59f762003e71608f6f738235cfec56509842eead
knguyen236 <knguyen236@student.hbuhsd.edu>
2024-12-11 21:49:04 +0000

    first github assignement 1.1

--- classwork.cpp
+++ classwork.cpp
@@ -3,10 +3,14 @@
 
 int main()
 {
-  // write code here
-  int age = 16
-  int pets = 0
-  int grade = 11
+  //SET age TO 10
+  int age = 16;
+  double price = 19.99;
+  bool hasPets = true; 
+
+std::cout << age << "\n" << price << std::endl << hasPets << std::endl;
+
+std::cout << "I am " << age << " years old.\n";
   
   return 0;
 }
